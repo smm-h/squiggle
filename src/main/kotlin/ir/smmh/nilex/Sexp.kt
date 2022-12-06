897563b6c9abb1a6b43098268e141026399c3d7d
SMMH <smmh72@gmail.com>
2022-12-06 17:18:37 +0330

    made Sexp customizable

--- src/main/kotlin/ir/smmh/nilex/Sexp.kt
+++ src/main/kotlin/ir/smmh/nilex/Sexp.kt
@@ -3,50 +3,37 @@ package ir.smmh.nilex
 import ir.smmh.lingu.Code
 import ir.smmh.lingu.Language
 import ir.smmh.lingu.Token
-import ir.smmh.lingu.Tokenizer
+import ir.smmh.nilex.NiLexLanguage.FilteredTokens
+import ir.smmh.nilex.NiLexLanguage.assertBalance
+import ir.smmh.nilex.NiLexLanguage.filterOut
+import ir.smmh.nilex.NiLexTokenizer.Companion.v
 
-object Sexp : Language.Construction<Token.Structure> {
+class Sexp(opener: String, closer: String, tokenizer: String, filter: (Token) -> Boolean) :
+    Language.Construction<Token.Structure> {
 
     override val construction = Code.Aspect<Token.Structure>("root")
 
-    private val tokenize: Tokenizer = NiLexTokenizerFactory.create(
-        """
-        streak '\t\n\r ' as whitespace
-        streak '[0-9]' as digits
-        streak '[A-Z][a-z][0-9]_' as id
-        keep '"' ... '"' as string
-        keep '//' ... '\n' as comment
-        keep '/*' ... '*/' as multiLineComment
-        verbatim '('
-        verbatim ')'
-        """
-    )
+    private val pOpen = v(opener)
+    private val pClose = v(closer)
+    private val tokenize = NiLexTokenizerFactory.create(tokenizer + "verbatim '$opener' verbatim '$closer'")
 
     private class Frame(val parent: Frame?) {
         val list: MutableList<Token.Structure> = ArrayList()
     }
 
-    private val assertBalance = NiLexLanguage.assertBalance("(", ")", NiLexLanguage.FilteredTokens)
-    private val pOpen = NiLexTokenizer.v("(")
-    private val pClose = NiLexTokenizer.v(")")
-
-    override val process: Code.Process = tokenize +
-            NiLexLanguage.filterOut("opener", "closer", "whitespace", "comment", "multiLineComment") +
-            assertBalance + { code ->
-        val queue = ArrayDeque((NiLexLanguage.FilteredTokens of code)!!)
-        var currentFrame: Frame = Frame(null)
-        while (queue.isNotEmpty()) {
-            val token = queue.removeFirst()
+    override val process: Code.Process = tokenize + assertBalance(opener, closer) + filterOut(filter) + { code ->
+        var curr: Frame = Frame(null)
+        for (token in FilteredTokens of code) {
             when (token.type.name) {
-                pOpen -> currentFrame = Frame(currentFrame)
+                pOpen -> curr = Frame(curr)
                 pClose -> {
-                    val previousFrame = currentFrame
-                    currentFrame = previousFrame.parent!!
-                    currentFrame.list.add(Token.Structure.Node(previousFrame.list))
+                    val prev = curr
+                    curr = prev.parent!!
+                    curr.list.add(Token.Structure.Node(prev.list))
                 }
-                else -> currentFrame.list.add(Token.Structure.Leaf(token))
+                else -> curr.list.add(Token.Structure.Leaf(token))
             }
         }
-        code[construction] = Token.Structure.Node(currentFrame.list)
+        code[construction] = Token.Structure.Node(curr.list)
     }
 }
--- src/test/kotlin/ir/smmh/nilex/SexpTest.kt
+++
@@ -1,8 +0,0 @@
-package ir.smmh.nilex
-
-import ir.smmh.lingu.Code
-import ir.smmh.lingu.Token
-
-fun main() {
-    println(Sexp.code("a(b(c(d \"e\" f g10 20 30 40h ())))").beConstructedInto<Token.Structure>())
-}
